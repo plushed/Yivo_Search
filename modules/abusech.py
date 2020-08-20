2e745327ce0d63f8aae2265d7b17728c6bfe2a2c
plushed <10122483+plushed@users.noreply.github.com>
2020-08-20 10:26:36 -0400

    Add files via upload

--- modules/abusech.py
+++ modules/abusech.py
@@ -1,9 +1,13 @@
 def abusech(args):
     args.k = args.k.rstrip("/")
+    args.k = args.k.lstrip('http://')
+    args.k = args.k.lstrip('https://')
     # Perform get request
     with open('./urlhaus.abuse.ch.txt', "r", newline='') as file:
         for line in file:
             line = line.rstrip()
             line = line.rstrip('/')
+            line = line.lstrip('http://')
+            line = line.lstrip('https://')
             if args.k == line:
                 print("True")
